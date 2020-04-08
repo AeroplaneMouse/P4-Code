c09aa516b148ccc7af82e004c9f4862f84dbad74
Daniel Fly <danielfly42@gmail.com>
2020-04-08 22:14:24 +0200

    WIP: New corona grammar

---
+++ CellularCompiler/CellularCompiler/Visitor/Corona/BuildAstVisitor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using CellularCompiler.Nodes;
+
+namespace CellularCompiler.Visitor.Corona
+{
+    //internal class BuildAstVisitor : CoronaBaseVisitor<ExpressionNode>
+    //{
+    //    //public override ExpressionNode VisitMain(CoronaParser.MainContext context)
+    //    //{
+    //    //    return Visit(context.grid());
+    //    //}
+
+    //    public override ExpressionNode VisitInfixExpr(CoronaParser.InfixExprContext context)
+    //    {
+    //        // Get infix expression node type
+    //        InfixExpressionNode node = context.op.GetText() switch
+    //        {
+    //            "+" => new AdditionNode(),
+    //            "-" => new SubstractionNode(),
+    //            "*" => new MultiplicationNode(),
+    //            "/" => new DivisionNode(),
+    //            _ => throw new ArgumentOutOfRangeException("context", "Unknown operator in switch statement - VisitInfixExpr")
+    //        };
+
+    //        // Visit the left and  of the node
+    //        node.Left = Visit(context.left);
+    //        node.Right = Visit(context.right);
+
+    //        return node;
+    //    }
+
+    //    public override ExpressionNode VisitStringExpr(CoronaParser.StringExprContext context)
+    //    {
+    //        return new StringNode(context.value.Text);
+    //    }
+
+
+    //    public override ExpressionNode VisitNumberExpr(CoronaParser.NumberExprContext context)
+    //    {
+    //        return new NumberNode(
+    //            double.Parse(context.value.Text, NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent)
+    //        );
+    //    }
+    //}
+}
