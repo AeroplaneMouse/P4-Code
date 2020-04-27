b309f16d11b4732f1274575019bcd9eff3bba341
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-04-27 10:23:07 +0200

    Symbol base class

---
+++ CellularCompiler/CellularCompiler/Models/Symbol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CellularCompiler.Models
+{
+    abstract public class Symbol 
+    {
+        public string Name { get; set; }
+
+        //Other things that are common for all symbols?
+    }
+}
