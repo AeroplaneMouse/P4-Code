57a5586800252a23564bef6dd9bcc2cf0da7b670
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-04-27 10:19:00 +0200

    ISymboltable

---
+++ CellularCompiler/CellularCompiler/Models/ISymbolTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CellularCompiler.Models
+{
+    interface ISymbolTable
+    {
+        void OpenScope();
+
+        void CloseScope();
+
+        Symbol RetrieveSymbol(string name);
+
+        void InsertSymbol(Symbol symbol);
+
+        void SetAttribute();
+    }
+}
