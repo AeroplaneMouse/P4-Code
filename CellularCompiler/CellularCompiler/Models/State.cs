06cb2829b2955d7da9f74e231a7c70821f6e1aff
Daniel Fly <danielfly42@gmail.com>
2020-04-28 12:06:27 +0200

    Added state model

---
+++ CellularCompiler/CellularCompiler/Models/State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CellularCompiler.Models
+{
+    class State
+    {
+        private static int _id;
+
+        public int ID { get; }
+        //public List<Member> Members;
+
+        public State()
+        {
+            ID = _id++;
+        }
+
+        public int GetLastID()
+        {
+            return _id - 1;
+        }
+    }
+}
