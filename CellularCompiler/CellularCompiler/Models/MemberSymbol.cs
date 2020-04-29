932d807bc952f1891806d66d3c2823e16e52142c
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-04-29 10:14:47 +0200

    added basic symbols Member and State.

---
+++ CellularCompiler/CellularCompiler/Models/MemberSymbol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CellularCompiler.Models
+{
+    class MemberSymbol<T> : Symbol
+    {
+        private T value;
+
+        private List<T> acceptedValues = new List<T>(); 
+        
+
+        public MemberSymbol(T initial, List<T> values)
+        {
+            acceptedValues = values;
+            value = initial;
+        }
+
+        public T GetValue()
+        {
+            return value;
+        }
+
+        public void SetValue(T value)
+        {
+            if (acceptedValues.Contains(value))
+                this.value = value;
+            else
+                throw new Exception("Invalid value");
+        }
+    }
+}
---
+++ CellularCompiler/CellularCompiler/Models/StateSymbol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CellularCompiler.Models
+{
+    class StateSymbol : Symbol
+    {
+        private List<MemberSymbol<object>> members = new List<MemberSymbol<object>>();
+
+        public void AddMember(MemberSymbol<object> member)
+        {
+            members.Add(member);
+        }
+
+        public MemberSymbol<object> RetrieveMember(string name)
+        {
+            return members.Find(s => s.Name.Equals(name));
+        }
+
+
+    }
+}
