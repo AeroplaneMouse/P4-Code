7ed68b5bda33ad7992b083c09301b4bb464207de
Daniel Fly <danielfly42@gmail.com>
2020-05-22 23:55:07 +0200

    Cleanup

--- CellularCompiler/CellularCompiler/CellularCompiler.cs
+++ CellularCompiler/CellularCompiler/CellularCompiler.cs
@@ -96,43 +96,5 @@ namespace CellularCompiler
 
             return evaluator;
         }
-
-        /// <summary>
-        /// An old example of how the math example works
-        /// </summary>
-        private static void CompileMath()
-        {
-            while (true)
-            {
-                Console.WriteLine("> ");
-                string input = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(input))
-                    throw new ArgumentNullException("input", "Argument was null or whitespace!");
-
-                var inputStream = new AntlrInputStream(new StringReader(input));
-                var lexer = new MathLexer(inputStream);
-                var tokenStream = new CommonTokenStream(lexer);
-                var parser = new MathParser(tokenStream);
-
-                try
-                {
-                    // Extract CST from antlr's shitty files
-                    var cst = parser.main();
-                    // Build AST from CST
-                    var ast = new Visitor.Math.BuildMathAstVisitor().VisitMain(cst);
-                    // Evaluate our newly compilled shitty code
-                    var value = new Visitor.Math.EvaluateMathExpressionVisitor().Visit(ast);
-
-                    Console.WriteLine($"= { value }");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-
-                Console.WriteLine();
-            }
-        }
     }
 }
